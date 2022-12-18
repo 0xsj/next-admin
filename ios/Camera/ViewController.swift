b878da446896343c6e671f15da819fed8d5d0601
sjtommylee <sjtommylee@gmail.com>
2022-12-18 14:33:50 -0800

    wip

---
+++ ios/Camera/ViewController.swift
@@ -0,0 +1,11 @@
+
+
+import Foundation
+import AVFoundation
+import UIKit
+
+
+let CustomButton: UIButton = {
+  let button = UIButton();
+  return button;
+}()
---
+++ ios/alfred.h
@@ -0,0 +1,17 @@
+//
+//  alfred.h
+//  alfred
+//
+//  Created by Tommy Lee on 12/18/22.
+//
+
+#import "AppDelegate.h"
+#import <React/RCTBridgeModule.h>
+#import <React>
+
+#ifndef alfred_h
+#define alfred_h
+
+
+
+#endif /* alfred_h */

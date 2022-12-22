f8f650637635164a985420b91cd3456678c6c6d6
sjtommylee <sjtommylee@gmail.com>
2022-12-21 20:41:37 -0800

    icon type switch

--- components/icon-button.tsx
+++ components/icon-button.tsx
@@ -10,9 +10,30 @@ export type IconType = "add" | "edit" | "delete" | "back" | "close" | "mail";
 
 export const IconButton: React.FC<Props> = (props) => {
   const { onPress, icon, type } = props;
+  const getIcon = () => {
+    switch (type) {
+      case "add":
+        return "plus";
+      case "edit":
+        return "edit";
+      case "delete":
+        return "trash";
+      case "back":
+        return "arrow-left";
+      case "close":
+        return "x";
+      case "mail":
+        return "mail";
+      default:
+        return "plus";
+    }
+  };
+
   return (
     <TouchableOpacity>
-      <Feather name="mail" size={24} color={"black"} />
+      <View>
+        <Feather name={getIcon()} size={24} color={"black"} />
+      </View>
     </TouchableOpacity>
   );
 };
--- screens/calendar.tsx
+++ screens/calendar.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Button, View } from "react-native";
 import { Box, Text } from "../atoms";
 import { RootStackScreenProps } from "types";
 import { IconButton } from "../components";
@@ -11,13 +11,29 @@ export function CalendarScreen({ navigation }: RootStackScreenProps<"Calendar">)
         <IconButton type={"add"} />
       </Box>
       <Box>
-        <IconButton type={"add"} />
+        <IconButton type={"edit"} />
       </Box>
       <Box>
-        <IconButton type={"add"} />
+        <IconButton type={"delete"} />
       </Box>
       <Box>
-        <IconButton type={"add"} />
+        <IconButton type={"back"} />
+      </Box>
+      <Box>
+        <IconButton type={"close"} />
+      </Box>
+      <Box>
+        <IconButton type={"back"} />
+      </Box>
+      <Box>
+        <IconButton />
+      </Box>
+      <Box>
+        <Button
+          color={"red"}
+          title="navigate to main screen"
+          onPress={() => navigation.navigate("Main")}
+        />
       </Box>
     </Box>
   );

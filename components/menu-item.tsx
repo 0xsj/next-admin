83fd0bdd1e0854fd08f6bcb2444e295375431bcd
sjtommylee <sjtommylee@gmail.com>
2022-12-25 11:07:08 -0800

    refactor, clean up

--- components/menu-item.tsx
+++
@@ -1,6 +0,0 @@
-import { Box, Text } from "@/atoms";
-export const MenuItem = () => {
-  <Box>
-    <Text>hi</Text>
-  </Box>;
-};
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,95 +1,24 @@
 import { SafeAreaView } from "react-native";
 import { Box, Container, Text } from "@/atoms";
 import { Feather } from "@expo/vector-icons";
-import { useEffect } from "react";
+import { menuItems } from "@/utils";
 export type Props = {};
-
-const menuItems = [
-  {
-    id: 1,
-    icon: "inbox",
-    label: "Inbox",
-    color: "white",
-  },
-  {
-    id: 2,
-    icon: "send",
-    label: "Sent",
-    color: "blue",
-  },
-  {
-    id: 3,
-    icon: "edit",
-    label: "Drafts",
-    color: "gray",
-  },
-  {
-    id: 4,
-    icon: "star",
-    label: "Starred",
-    color: "yellow",
-  },
-  {
-    id: 5,
-    icon: "send",
-    label: "Archive",
-    color: "green",
-  },
-  {
-    id: 6,
-    icon: "trash-2",
-    label: "Trash",
-    color: "red",
-  },
-];
+export type MenuItems = "inbox" | "send" | "edit" | "star" | "send" | "trash-2";
 
 export const Sidebar: React.FC = () => {
-  const renderMenuItems = () => {
-    menuItems.map((item, index) => {
-      // console.log(item);
-    });
-  };
-  renderMenuItems();
   return (
     <Container>
       <Box flex={1}>
         <SafeAreaView>
-          <Box flexDirection="row">
-            <Feather name="inbox" color={"white"} size={16} />
-            <Text color={"$foreground"} fontSize={16}>
-              Inbox
-            </Text>
-          </Box>
-          <Box flexDirection="row">
-            <Feather name="send" color={"blue"} size={16} />
-            <Text color={"$foreground"} fontWeight={"bold"} fontSize={16}>
-              Sent
-            </Text>
-          </Box>
-          <Box flexDirection="row">
-            <Feather name="edit" color={"gray"} size={16} />
-            <Text color={"$foreground"} fontWeight={"bold"} fontSize={16}>
-              Drafts
-            </Text>
-          </Box>
-          <Box flexDirection="row">
-            <Feather name="star" color={"yellow"} size={16} />
-            <Text color={"$foreground"} fontWeight={"bold"} fontSize={16}>
-              Starred
-            </Text>
-          </Box>
-          <Box flexDirection="row">
-            <Feather name="send" color={"green"} size={16} />
-            <Text color={"$foreground"} fontWeight={"bold"} fontSize={16}>
-              Archive
-            </Text>
-          </Box>
-          <Box flexDirection="row">
-            <Feather name="trash-2" color={"red"} size={16} />
-            <Text color={"$foreground"} fontWeight={"bold"} fontSize={16}>
-              Trash
-            </Text>
-          </Box>
+          {menuItems.map((item, index) => {
+            const iconName = item.icon as MenuItems;
+            return (
+              <Box key={`${item.id}-${index}`}>
+                <Feather name={iconName} color={item.color} size={16} />
+                <Text>{item.label}</Text>
+              </Box>
+            );
+          })}
         </SafeAreaView>
       </Box>
     </Container>

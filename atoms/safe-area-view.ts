eee75560c9356eaed653a4d4ff277515b1fbe846
sjtommylee <sjtommylee@gmail.com>
2023-01-01 10:21:32 -0800

    safe area view

--- atoms/safe-area-view.ts
+++ atoms/safe-area-view.ts
@@ -1,1 +1,6 @@
-export {};
+import { Theme } from "theme";
+import { SafeAreaView as NativeSafeAreaView, ViewProps } from "react-native";
+import { createBox } from "@shopify/restyle";
+
+export const SafeAreaView = createBox<Theme, ViewProps>(NativeSafeAreaView);
+export type SafeAreaViewProps = React.ComponentProps<typeof SafeAreaView>;

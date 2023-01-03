04ac2099656adce9fc2220ffc4aac299a2f3e4ee
sjtommylee <sjtommylee@gmail.com>
2023-01-03 06:37:47 -0800

    sidebar item

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,37 +1,35 @@
 import { SafeAreaView } from "react-native";
 import { Box, Container, Text } from "@/atoms";
-import { Feather } from "@expo/vector-icons";
 import { menuItems } from "@/utils";
+import { SidebarItem } from "./sidebar-item";
+
 export type Props = {};
 export type MenuItems = "inbox" | "send" | "edit" | "star" | "send" | "trash-2";
 
 export const Sidebar: React.FC = () => {
   return (
-    <Container>
+    <Container justifyContent={"space-between"}>
       <Box>
         <SafeAreaView>
           <Text fontSize={14} padding="sm">
             Folders
           </Text>
-
           {menuItems.map((item, index) => {
             const iconName = item.icon as MenuItems;
             return (
-              <Box
-                alignItems={"center"}
-                width={"100%"}
-                flexDirection={"row"}
+              <SidebarItem
                 key={`${item.id}-${index}`}
-              >
-                <Box ml="md">
-                  <Feather name={iconName} color={item.color} size={20} />
-                </Box>
-                <Text fontSize={14} fontWeight="500" padding="sm">
-                  {item.label}
-                </Text>
-              </Box>
+                color={item.color}
+                iconName={iconName}
+                label={item.label}
+              />
             );
           })}
+        </SafeAreaView>
+      </Box>
+      <Box>
+        <SafeAreaView>
+          <SidebarItem color={"black"} iconName={"settings"} label={"Settings"} />
         </SafeAreaView>
       </Box>
     </Container>

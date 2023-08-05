1ccdd81bc35e3ee45db5f6bddaa5482506110a7b
sjtommylee <sjtommylee@gmail.com>
2023-08-05 01:45:19 -0700

    chore: commit save point

--- build.sh
+++ build.sh
@@ -3,40 +3,53 @@
 tsServices=("authentication" "account" "profile" "token")  # Add your TS service names here
 gatewayServices=("authentication" "account" "profile" "token")
 
-function cleanup() {
-  folderPath="$1/generated"
-  echo "$folderPath"
-  if [ -d "$folderPath" ]; then
-    rm -r "$folderPath"
-  fi
-  mkdir -p "$folderPath"
-}
-
-function cleanGeneratedFolders() {
-  for service in "${tsServices[@]}"; do
-    cleanup "services/$service"
-  done
-
-  for service in "${gatewayServices[@]}"; do
-    cleanup "gateway/$service"
-  done
-}
-
-
 function generateTS() {
-  npm run build:proto --prefix "$1"  # Execute npm command in the specified directory
+  local services_dir="$1"
+  
+  # Loop through each service directory
+  for service in "${tsServices[@]}"; do
+    local service_dir="$services_dir/$service"
+    
+    # Check if the service directory exists and has a build script
+    if [ -d "$service_dir" ] && [ -f "$service_dir/build.sh" ]; then
+      echo "Building $service in directory: $service_dir..."
+      (cd "$service_dir" && npm run build:proto)
+    else
+      echo "Build script not found for $service in directory: $service_dir. Skipping..."
+    fi
+  done
 }
 
-# generate code for TS services
-for service in "${tsServices[@]}"; do
-  generateTS "services/$service"
-  echo "Generated proto for TS service: $service"
-done
+function cleanTS() {
+  local services_dir="$1"
+  
+  # Loop through each service directory
+  for service in "${tsServices[@]}"; do
+    local service_dir="$services_dir/$service"
+    
+    # Check if the service directory exists
+    if [ -d "$service_dir" ]; then
+      echo "Cleaning $service in directory: $service_dir..."
+      
+      # Attempt to delete the directory and its contents
+      rm -rf "$service_dir"
+      
+      if [ $? -eq 0 ]; then
+        echo "Successfully cleaned $service in directory: $service_dir."
+      else
+        echo "Failed to clean $service in directory: $service_dir. Please check permissions."
+      fi
+    else
+      echo "$service directory not found in directory: $service_dir. Skipping..."
+    fi
+  done
+}
 
-# generate code for gateway services
-for service in "${gatewayServices[@]}"; do
-  generateTS "gateway/$service"  # Assuming you have a separate directory for each gateway service
-  echo "Generated proto for gateway service: $service"
-done
+# Update the paths to your actual directories
+generateTS "services/account"
+generateTS "services/authentication"
+generateTS "services/profile"
+generateTS "services/token"
+generateTS "services/gateway"
 
 sleep 0.5

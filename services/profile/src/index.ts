677398a60169744d6684aa9778b4d95165ce1b1a
sjtommylee <sjtommylee@gmail.com>
2023-08-05 16:10:31 -0700

    chore: commit save point

--- services/profile/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import http from "http";
-const app = express();
-
-const server = http.createServer(app);
-
-server.listen(8080, () => {
-  console.log("server running");
-});
